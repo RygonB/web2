3203cf07a1296a9a634f502f59749cdb61f69e84
Rayane <rayane.belbachir@student.vinci.be>
2023-09-28 17:11:36 +0200

    1.2 : app middleware

---
+++ exercices/1.2/routes/films.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+
+const FILMS = [
+    {
+        id: 1,
+        title: "Avatar",
+        duration: 120,
+        budget: 250000000,
+        link: 'https://www.rottentomatoes.com/m/avatar_the_way_of_water',
+    },
+
+    {
+        id: 1,
+        title: "Avatar",
+        duration: 120,
+        budget: 250000000,
+        link: 'https://www.rottentomatoes.com/m/avatar_the_way_of_water',
+    },
+
+    {
+        id: 1,
+        title: "Avatar",
+        duration: 120,
+        budget: 250000000,
+        link: 'https://www.rottentomatoes.com/m/avatar_the_way_of_water'
+    }
+];
+
+router.get('/', (req, res, next) => {
+    res.json(FILMS);
+});
+
+router.post('/',(req,res,next)=>{
+    res.redirect('/films');
+})
+
+router.delete('/',(req,res,next)=>{
+    res.redirect('/films');
+})
+module.exports = router;
