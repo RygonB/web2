7ffb647862c927cff5c3c1f04a4c636de45cba98
Rayane <rayane.belbachir@student.vinci.be>
2023-11-13 12:13:59 +0100

    2.4.1 : run action within timer

--- exercices/2.4.1/index.js
+++ exercices/2.4.1/index.js
@@ -1,6 +1,33 @@
-const button = document.querySelector("#button");
-
 const delayTime = 4;
 
+let clicks = 0;
+let startTime;
+let timeReference;
+const clickButton = document.getElementById("clickButton");
 
+clickButton.addEventListener("mouseenter", () => {
+  startTime = new Date().getTime();
+  clicks = 0;
+  clickButton.addEventListener("click", handleClick);
+  timeReference = setTimeout(checkGameResult, 5000);
+});
 
+function handleClick() {
+  clicks++;
+  if (clicks === 10) {
+    clearTimeout(timeReference);
+    checkGameResult();
+  }
+}
+
+function checkGameResult() {
+  clickButton.removeEventListener("click", handleClick);
+  const endTime = new Date().getTime();
+  const elapsedTime = endTime - startTime;
+
+  if (clicks === 10) {
+    clickButton.innerHTML = `You win! You clicked 10 times within ${elapsedTime} ms`;
+  } else {
+    clickButton.innerHTML = "Game over, you did not click 10 times within 5s!";
+  }
+}
